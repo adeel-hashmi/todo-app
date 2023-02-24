5d7f1406fbe52ce730b066d696daf25b89da34d7
AqeelHashmi01 <aqeelhashmi26@gmail.com>
2023-02-24 10:30:41 +0500

    testing env (counter app)

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0)
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+  const handleDecrement = () => {
+    setCount(count - 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Counter App</Text>
+      <Text style={styles.count}>{count}</Text>
+      <Button style={styles.button} title="-" onPress={handleDecrement} />
+      <Button style={styles.button} title="+" onPress={handleIncrement} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  count: {
+    fontSize: 100,
+    marginBottom: 20,
+  },
+  button: {
+    includeFontPadding: 30,
+    marginBottom: 20,
+  }
 });
